87eb0e0950ba9159629a3c8176382172a4da2814
Henrique <heenriquecds@gmail.com>
2025-07-25 17:02:10 +0100

    chore: update phone and address

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -19,7 +19,7 @@ export const Footer = () => {
             <h2 className="text-4xl mt-16">
               {i18n.t("commons.footer.startBy")}{" "}
               <a
-                href="https://wa.me/5511950645574"
+                href="https://wa.me/353831383418"
                 className="mt-8 text-green-400 underline cursor-pointer"
               >
                 {i18n.t("commons.footer.sayingHi")}
